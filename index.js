1117bf01f38cead797b3074bb48d7f402e725ea1
Daniel <gaspardani87@gmail.com>
2024-12-22 00:27:09 +0100

    Fileok feltöltése

---
+++ index.js
@@ -0,0 +1,20 @@
+function updateCountdown() {
+    const most = new Date();
+    const karacsony = new Date('December 25, 2025 00:00:00');
+    const kulonbseg = karacsony - most;
+
+    const nap = Math.floor(kulonbseg / (1000 * 60 * 60 * 24));
+    const ora = Math.floor((kulonbseg % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const perc = Math.floor((kulonbseg % (1000 * 60 * 60)) / (1000 * 60));
+    const masodperc = Math.floor((kulonbseg % (1000 * 60)) / 1000);
+
+    document.getElementById('countdown').innerHTML = 
+        `${nap} nap ${ora} óra ${perc} perc ${masodperc} másodperc`;
+
+    if (kulonbseg < 0) {
+        clearInterval(interval);
+        document.getElementById('countdown').innerHTML = "Boldog Karácsonyt!";
+    }
+}
+
+const interval = setInterval(updateCountdown, 1000);
